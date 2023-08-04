dc60144e22d287991d2ce22ba62cf191f50d80e6
Kleber Fukushima <kleber.jhun@gmail.com>
2023-08-04 12:11:34 -0300

    commit inicial com duvida

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
-const form = document.getElementById('form-numeros');
-const numeroA = document.getElementById('numero-a');
-const numeroB = document.getElementById('numero-b');
+$(document).ready(function() {
+    $('li').click(function() {
+        if($(this).hasClass('tarefa-nao-feita')) {
+         $(this).removeClass('tarefa-nao-feita').addClass('tarefa-feita');
+        } else {
+         $(this).removeClass('tarefa-feita').addClass('tarefa-nao-feita');
+        }
+     })
 
-function comparacao (){
-    if(numeroB.value > numeroA.value) {
-        return true
-    } else {
-        return false
-    }
-}
-
-
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-
-    if(comparacao()) {
-        document.querySelector('.success-msg').style.display = 'block';
-        document.querySelector('.fail-msg').style.display = 'none';         
-
-    } else {
-        document.querySelector('.fail-msg').style.display = 'block';
-        document.querySelector('.success-msg').style.display = 'none';        
-    }
+     $('form').on('submit', function(e) {
+        e.preventDefault();
+        const nomeTarefa = $('#nome-tarefa').val();
+        let novaTarefa = $(`<li class="tarefa-nao-feita">${nomeTarefa}</li>`);
+    
+        $(novaTarefa).appendTo('ul');
+        
+        $('#nome-tarefa').val('');
+    })  
 })
-
